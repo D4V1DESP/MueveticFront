d2ed3268989e0a882801795446be2660b27c6ef7
Daniel-Machuca <115093834+Daniel-Machuca@users.noreply.github.com>
2023-11-22 09:32:50 +0100

    primera version del form de 2FA (faltan cosas)

---
+++ MueveTIC/src/app/authentication.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {AuthenticationResponse} from "../app/authentication-response";
+import {VerificationRequest} from "../app/verification-request";
+import {AuthenticationRequest} from "../app/authentication-request";
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  /* private baseUrl = 'http://localhost:8080/api/v1/auth'*/ 
+
+  constructor(private http: HttpClient) { }
+
+/*
+  registro(registerRequest: RegisterRequest) {
+    return this.http.post<AuthData>
+    (`${this.baseUrl}/registro-usuarios`, registro-usuarios.userData);
+  }*/
+
+  
+  login(authRequest: AuthenticationRequest) {
+    return this.http.post<AuthenticationResponse>
+    /*(`${this.baseUrl}/authenticate`, authRequest);*/
+  }
+
+  verifyCode(verificationRequest: VerificationRequest) {
+    return this.http.post<AuthenticationResponse>
+    /*(`${this.baseUrl}/verify`, verificationRequest);*/
+  }
+}
+
